fe0ddadec49733e4541ac3229fd7d61b13fb0891
Chris Berthiaume <chrisbee@uw.edu>
2021-06-24 13:50:12 -0700

    Add 'long' argument to build script to optionally embed commit

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Build cruisereplay command-line tool for 64-bit MacOS and Linux
 
-VERSION=$(git describe --long --dirty --tags)
+if [[ $1 == "long" ]]; then
+  VERSION=$(git describe --long --dirty --tags)
+else
+  VERSION=$(git describe --dirty --tags)
+fi
+
 GOOS=darwin GOARCH=amd64 go build -o "cruisereplay-${VERSION}-darwin-amd64" main.go || exit 1
 GOOS=linux GOARCH=amd64 go build -o "cruisereplay-${VERSION}-linux-amd64" main.go || exit 1
 #gzip "cruisereplay-${VERSION}-darwin-amd64"
